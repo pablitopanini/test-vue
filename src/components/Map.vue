5da9f67805f0b7f0cb616d0a10c37eb23171cb7e
Pavel Panin <pavel99499@gmail.com>
2020-02-04 11:47:20 +0300

    final

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -31,49 +31,96 @@
         <vl-source-osm></vl-source-osm>
       </vl-layer-tile>
 
-      <vl-layer-tile id="wms" :extent="[-13884991, 2870341, -7455066, 6338219]">
-        <vl-source-wms
-          :url="'https://ahocevar.com/geoserver/wms'"
-          :cross-origin="'anonymous'"
-          :server-type="'geoserver'"
-          :transition="0"
-          :layers="'topp:states'"
-          :tiled="true"
-        ></vl-source-wms>
-      </vl-layer-tile>
+      <template
+        v-for="({ type, sourceProps, visible, layerProps, opacity },
+        index) in config"
+      >
+        <template v-if="visible">
+          <vl-layer-tile v-if="type === 'wms'" :key="index" v-bind="layerProps">
+            <vl-source-wms
+              v-bind="sourceProps"
+              :transition="parseInt(opacity)"
+            ></vl-source-wms>
+          </vl-layer-tile>
 
-      <vl-layer-vector>
-        <vl-source-vector
-          :url="
-            'https://openlayers.org/en/latest/examples/data/geojson/countries.geojson'
-          "
-        ></vl-source-vector>
-      </vl-layer-vector>
+          <vl-layer-vector v-if="type === 'wfs'" :key="index">
+            <vl-source-vector v-bind="sourceProps"></vl-source-vector>
+          </vl-layer-vector>
+        </template>
+      </template>
     </vl-map>
-    <!-- <div style="padding: 20px">
+
+    <div>
+      Слои <br />
+      <template v-for="(layer, index) in config">
+        <input type="checkbox" :key="index" v-model="layer.visible" />
+        {{ layer.name }}, opacity:
+        <input
+          type="number"
+          min="0"
+          max="1"
+          step="0.1"
+          :key="'n' + index"
+          v-model="layer.layerProps.opacity"
+        />
+        <button v-if="index > 0" :key="'up' + index" @click="up(layer, index)">
+          ↑
+        </button>
+        <button
+          v-if="index < config.length - 1"
+          :key="'down' + index"
+          @click="down(layer, index)"
+        >
+          ↓
+        </button>
+        <br :key="'br' + index" />
+      </template>
+    </div>
+    <br />
+    <div>
       Zoom: {{ zoom }}<br />
       Center: {{ center }}<br />
       Rotation: {{ rotation }}<br />
       My geolocation: {{ geolocPosition }}
-    </div> -->
+    </div>
   </div>
 </template>
 
 <script>
+function arrayMove(arr, old_index, new_index) {
+  if (new_index >= arr.length) {
+    var k = new_index - arr.length + 1;
+    while (k--) {
+      arr.push(undefined);
+    }
+  }
+  arr.splice(new_index, 0, arr.splice(old_index, 1)[0]);
+  return arr;
+}
+
 export default {
   name: "Map",
+  props: ["initial-config"],
   data() {
     return {
       zoom: 2,
       center: [0, 0],
       rotation: 0,
-      geolocPosition: undefined
+      geolocPosition: undefined,
+      config: this.initialConfig.map(l => ({ ...l, visible: true }))
     };
   },
   watch: {
     geolocPosition: function(val) {
       this.center = val;
-      //   this.zoom = 14;
+    }
+  },
+  methods: {
+    up: function(el, idx) {
+      arrayMove(this.config, idx, idx - 1);
+    },
+    down: function(el, idx) {
+      arrayMove(this.config, idx, idx + 1);
     }
   }
 };
